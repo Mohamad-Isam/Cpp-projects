a2bfeb1aabbd91d4d3108810d95815f12f7b9c61
Mohamed Isam <116897631+Mohamad-Isam@users.noreply.github.com>
2023-04-13 04:43:14 +0200

    Update MathGame.cpp

--- MathGame.cpp
+++ MathGame.cpp
@@ -2,20 +2,28 @@
 using namespace std;
 
 enum enMathOperation {ADD = 1, SUB = 2, MUL = 3, DIV = 4, MIX = 5};
-enum enDifficultyLevel {EASY = 1, MEDIUM = 2, HARD = 3, MIX = 4};
+enum enDifficultyLevel {EASY = 1, MEDIUM = 2, HARD = 3, MIX2 = 4};
 
 struct stPlayerChoices{
-int level;
-int mathOp;
-int numberOfQuestions;
+    int level;
+    int mathOp;
+    int numberOfQuestions;
 
 };
 
 struct stQuestionElements{
-int number1;
-int number2;
+    int number1;
+    int number2;
 
-char op;
+    char op;
+};
+
+struct stGameInfo{
+    int numberOfQuestions = 0;
+    string questionLevel;
+    string mathOperation;
+    int rightAnswers = 0;
+    int wrongAnswers = 0;
 };
 
 stPlayerChoices readPlayingChoices()
@@ -27,7 +35,7 @@ stPlayerChoices readPlayingChoices()
     cin >> choices.level;
     cout << "Enter operation type : [1] add, [2] sub, [3] mul, [4] div, [5] mix ? ";
     cin >> choices.mathOp;
-    
+
     return choices;
 }
 int randomNumber(int From, int To)
@@ -54,7 +62,7 @@ stQuestionElements generateQuestions(stPlayerChoices choices)
         question.number1 = randomNumber(100, 10000);
         question.number2 = randomNumber(100, 10000);
     }
-    else if(choices.level == enDifficultyLevel::MIX)
+    else if(choices.level == enDifficultyLevel::MIX2)
     {
         question.number1 = randomNumber(1, 10000);
         question.number2 = randomNumber(1, 10000);
@@ -71,9 +79,9 @@ stQuestionElements generateQuestions(stPlayerChoices choices)
         question.op = '/';
     else if(choices.mathOp == enMathOperation::MIX)
         question.op = v[randomNumber(0, 3)];
-    
+
     return question;
-    
+
 }
 
 int correctAnswerOfQuestion(stQuestionElements question)
@@ -104,13 +112,8 @@ int readPlayerAnswer()
 {
     int answer;
     cin >> answer;
-}
 
-
-string resultToString(int result)
-{
-    vector<string> v = {"WIN", "LOSS", "DRAW"};
-    return v[result - 1];
+    return answer;
 }
 
 void lossScreen()
@@ -127,6 +130,88 @@ void clearScreen()
 {
     system("cls");
     system("color 0F");
+}
+
+string mathOpToString(stPlayerChoices choices)
+{
+    switch(choices.mathOp)
+    {
+    case enMathOperation::ADD:
+        return "ADD";
+    case enMathOperation::SUB:
+        return "SUB";
+    case enMathOperation::MUL:
+        return "MUL";
+    case enMathOperation::DIV:
+        return "DIV";
+    case enMathOperation::MIX:
+        return "MIX";
+    }
+    return "MIX";
+}
+string levelToString(stPlayerChoices choices)
+{
+    switch(choices.level)
+    {
+    case enDifficultyLevel::EASY:
+        return "EASY";
+    case enDifficultyLevel::MEDIUM:
+        return "MEDIUM";
+    case enDifficultyLevel::HARD:
+        return "HARD";
+    case enDifficultyLevel::MIX2:
+        return "MIX";
+    }
+    return "MIX";
+}
+
+stGameInfo playBattle()
+{
+    stPlayerChoices choices;
+    stQuestionElements question;
+    stGameInfo gameInfo;
+    choices = readPlayingChoices();
+
+    for(int i = 1; i <= choices.numberOfQuestions; i++)
+    {
+        question = generateQuestions(choices);
+        //read gameInfo
+        gameInfo.numberOfQuestions = choices.numberOfQuestions;
+        gameInfo.questionLevel = levelToString(choices);
+        gameInfo.mathOperation = mathOpToString(choices);
+
+        cout << "\n\nQuestion [" << i << "/ " << choices.numberOfQuestions << "]\n";
+        viewQuestion(question);
+        int result = correctAnswerOfQuestion(question);
+        int answer = readPlayerAnswer();
+        if(answer == result)
+        {
+            gameInfo.rightAnswers++;
+            cout << "Correct Answer!!\n\n";
+            winScreen();
+        }
+        else
+        {
+            gameInfo.wrongAnswers++;
+            cout << "Wrong answer!!\n";
+            cout << "The right answer is  " << result << '\n';
+            lossScreen();
+        }
+    }
+     cout << "Right answers  = " << gameInfo.rightAnswers;
+     cout << "\nWrong answers  = " << gameInfo.wrongAnswers;
+    return gameInfo;
+}
+
+void playGame()
+{
+    do
+    {
+        clearScreen();
+        stGameInfo gameInfo = playBattle();
+        
+        
+    }while(playerChoice == 'y' || playerChoice == 'Y');
 }
 
 int main()
